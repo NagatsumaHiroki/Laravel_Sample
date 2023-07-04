352c58fa17fb1baed283e22429fa5955e14334b3
Nagastuma <act1911@gmail.com>
2023-07-04 10:58:54 +0900

    cssのクラスの引き渡し

--- multi-ec/resources/views/components/tests/card.blade.php
+++ multi-ec/resources/views/components/tests/card.blade.php
@@ -4,7 +4,10 @@
     'message' => '',
     'content' =>  ''
 ])
-<div class="broder-2 shadow-md w-1/4 p-2">
+{{-- cssの引き渡しを設定 --}}
+<div {{ $attributes->merge([
+ 'class' => "broder-2 shadow-md w-1/4 p-2"
+]) }}>
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
--- multi-ec/resources/views/tests/component-test1.blade.php
+++ multi-ec/resources/views/tests/component-test1.blade.php
@@ -2,4 +2,5 @@
     <x-slot name="header">ヘッダ-1</x-slot>
     コンポーネントテスト1
     <x-tests.card title="タイトル1" content="aaaaa" :message="$message"/>
+    <x-tests.card title="CSS変更" class="bg-red-300"/>
 </x-tests.app>
