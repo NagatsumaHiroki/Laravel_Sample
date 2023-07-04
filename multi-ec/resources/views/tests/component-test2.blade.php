0a8ea519111a39db5c73536aac7a79db4488d035
Nagastuma <act1911@gmail.com>
2023-07-04 11:27:37 +0900

    クラスベースのコンポーネント作成

--- multi-ec/resources/views/tests/component-test2.blade.php
+++ multi-ec/resources/views/tests/component-test2.blade.php
@@ -2,4 +2,7 @@
     <x-slot name="header">ヘッダ-2</x-slot>
     コンポーネントテスト2
     <x-tests.card title="タイトル2" content="aaaaa"/>
+    <x-test-class-base classBaseMessage="メッセージです" />
+    <div></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
 </x-tests.app>
