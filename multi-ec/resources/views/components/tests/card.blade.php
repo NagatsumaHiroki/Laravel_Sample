076087a74db80f03aea3869c0cbc87134961f758
Nagastuma <act1911@gmail.com>
2023-07-04 10:49:37 +0900

    props設定

--- multi-ec/resources/views/components/tests/card.blade.php
+++ multi-ec/resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+{{-- 初期値を設定 --}}
+@props([
+    'title' => '',
+    'message' => '',
+    'content' =>  ''
+])
 <div class="broder-2 shadow-md w-1/4 p-2">
     <div>{{ $title }}</div>
     <div>画像</div>
